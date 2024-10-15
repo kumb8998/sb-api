47448bae3171c5196b327fede454ff445a615753
kumb8998 <aniket.kumbhar8998.com>
2024-10-15 19:14:23 +0530

    My first commit

---
+++ src/main/java/com/sb_api/SbApiApplication.java
@@ -0,0 +1,14 @@
+package com.sb_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbApiApplication {
+
+	public static void main(String[] args) {
+		System.out.println("********Welcome to API***********");
+		SpringApplication.run(SbApiApplication.class, args);
+	}
+
+}
